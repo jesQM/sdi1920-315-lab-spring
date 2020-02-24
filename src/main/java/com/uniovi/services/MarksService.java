82915d57e88a70161ca3f841ec1ddc265fc1fc69
jesQM <jezuquema@gmail.com>
2020-02-24 20:37:46 +0100

    SDI-SpringBoot-Sesión-4.1-Sesiones

--- src/main/java/com/uniovi/services/MarksService.java
+++ src/main/java/com/uniovi/services/MarksService.java
@@ -1,7 +1,11 @@
 package com.uniovi.services;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,23 +17,33 @@ import com.uniovi.repositories.MarksRepository;
 public class MarksService {
 	@Autowired
 	private MarksRepository marksRepository;
-    
-    public List<Mark> getMarks() {
-    	List<Mark> marks = new ArrayList<Mark>();
-    	marksRepository.findAll().forEach(marks::add);
-    	return marks;
-    }
-    
-    public Mark getMark(Long id) {
-    	return marksRepository.findById(id).get();
-    }
-    
-    public void addMark(Mark mark) {
-    	// Si en Id es null le asignamos el ultimo + 1 de la lista
-    	marksRepository.save(mark);
-    }
-    
-    public void deleteMark(Long id) {
-    	marksRepository.deleteById(id);
-    }
+
+	@Autowired
+	private HttpSession httpSession;
+
+	public List<Mark> getMarks() {
+		List<Mark> marks = new ArrayList<Mark>();
+		marksRepository.findAll().forEach(marks::add);
+		return marks;
+	}
+
+	public Mark getMark(Long id) {
+		Set<Mark> consultedList = (Set<Mark>) httpSession.getAttribute("consultedList");
+		if (consultedList == null)
+			consultedList = new HashSet<Mark>();
+		
+		Mark obtainedmark = marksRepository.findById(id).get();
+		consultedList.add(obtainedmark);
+		httpSession.setAttribute("consultedList", consultedList);
+		return obtainedmark;
+	}
+
+	public void addMark(Mark mark) {
+		// Si en Id es null le asignamos el ultimo + 1 de la lista
+		marksRepository.save(mark);
+	}
+
+	public void deleteMark(Long id) {
+		marksRepository.deleteById(id);
+	}
 }
